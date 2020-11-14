728b7ecaca3fb240007c9c1154213b1d53f122ba
Jeff Ward <jsward.17@gmail.com>
2020-11-13 19:58:22 -0500

    fetch research

--- test/utils/getAddressData.test.ts
+++ test/utils/getAddressData.test.ts
@@ -1,11 +1,12 @@
 import getAddressData from '../../src/utils/getAddressData'
 import dataUnsorted from '../__mocks__/ethplorer-response-tokens-unsorted-mock'
 
-// Would need to do a deeper dive on mocking window.fetch API
-// https://www.leighhalliday.com/mock-fetch-jest
+// Would need to do a much deeper dive on fetch mocking
+// Found this in my searching
+// https://kentcdodds.com/blog/stop-mocking-fetch
 
 test('calls the API and has a top level ETH property in the response', async () => {
-  // let apiResult = await getAddressData('0x83F2362A94d3FA99905DAb8e52dC1A74Dd22f31b')
-  // expect(getAddressData('0x83F2362A94d3FA99905DAb8e52dC1A74Dd22f31b')).toHaveProperty('ETH');
+  // let apiResult = await getAddressData(dataUnsorted.address)
+  // expect(apiResult).toHaveProperty('ETH');
   expect(true).toBe(true);
 });

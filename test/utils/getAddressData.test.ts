0c56e7bea49df423848b880e3badaeb018f58e1f
Jeff Ward <jsward.17@gmail.com>
2020-11-15 08:19:41 -0500

    getdata test works now

--- test/utils/getAddressData.test.ts
+++ test/utils/getAddressData.test.ts
@@ -1,12 +1,11 @@
 import getAddressData from '../../src/utils/getAddressData'
 import dataUnsorted from '../__mocks__/ethplorer-response-tokens-unsorted-mock'
 
-// Would need to do a much deeper dive on fetch mocking
-// Found this in my searching
+// Browser mocking sucks - swtiched window.fetch to axios
 // https://kentcdodds.com/blog/stop-mocking-fetch
 
 test('calls the API and has a top level ETH property in the response', async () => {
-  // let apiResult = await getAddressData(dataUnsorted.address)
-  // expect(apiResult).toHaveProperty('ETH');
-  expect(true).toBe(true);
+  let apiResult = await getAddressData(dataUnsorted.address)
+  expect(apiResult).toHaveProperty('ETH');
+  // expect(true).toBe(true);
 });
